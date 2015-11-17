840046ec5e685b477fc2d9bce2bcccc6b6c5ed0f
Jaeger <chjie.jaeger@gmail.com>
2015-11-17 17:18:35 +0800

    modify BaseRecycleAdapter

--- app/src/main/java/com/jaeger/listenrain/adapter/ArticleItemAdapter.java
+++ app/src/main/java/com/jaeger/listenrain/adapter/ArticleItemAdapter.java
@@ -2,7 +2,6 @@ package com.jaeger.listenrain.adapter;
 
 import android.content.Context;
 import android.net.Uri;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
